a714ffe8be3a175689e9d2bb8c04fcf99fb2cb1a
shenkzjay <senksjay@gmail.com>
2025-09-28 12:58:18 +0100

    fix:remove dotenv file causing edge runtime error

--- server/trpc.ts
+++ server/trpc.ts
@@ -1,27 +1,13 @@
 import { initTRPC } from "@trpc/server";
+import { db } from "../src/db/db.js";
 
 const t = initTRPC.create();
 
-export const router = t.router;
-export const publicProcedure = t.procedure;
-
-/**
- * This is how you'd find a user in a database
- * based on their session cookie or token
- */
-const getUser = () => {
-  // ...
-  return { name: "alex" };
-};
-
-/**
- * This is your actual context that will be passed to the router
- * and all of your procedures.
- *
- * @link https://trpc.io/docs/context
- */
-export const createContext = () => {
+export const createContext = async () => {
   return {
-    user: getUser(),
+    db: db.get(),
   };
 };
+
+export const router = t.router;
+export const publicProcedure = t.procedure;
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,7 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle, NeonHttpDatabase } from "drizzle-orm/neon-http";
-import * as schema from "./schema.ts";
-import "dotenv/config";
+import * as schema from "./schema.js";
+// import "dotenv/config";
 
 let _drizzleInstance: NeonHttpDatabase<typeof schema> | undefined;
 
