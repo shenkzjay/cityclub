fd67536a1e15b26348b4132d7e33b216863663db
shenkzjay <senksjay@gmail.com>
2025-09-28 15:31:58 +0100

    fix:error still persist

--- api/trpc/[trpc]/route.ts
+++ api/trpc/[trpc]/route.ts
@@ -8,8 +8,8 @@
 //   router: appRouter,
 //   createContext,
 // });
-import { appRouter } from "../../../server/index.ts";
-import { createContext } from "../../../server/trpc.ts";
+import { appRouter } from "../../../server/index.js";
+import { createContext } from "../../../server/trpc.js";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 export const config = {
